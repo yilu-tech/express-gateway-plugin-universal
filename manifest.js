82b1ec8238580270ed4960f067b3d8e3f2ab52c3
lishu <lishu@yilu.co>
2018-05-27 19:39:05 +0800

    移除universal.config

--- manifest.js
+++ manifest.js
@@ -5,7 +5,6 @@ module.exports = {
   init: function (pluginContext) {
   
 
-    pluginContext.registerPolicy(require('./lib/policies/universal'));//注册政策
     pluginContext.registerPolicy(require('./lib/policies/api-match'));//注册api-match政策
     pluginContext.registerPolicy(require('./lib/policies/api-auth'));//注册api-auth政策
     pluginContext.registerPolicy(require('./lib/policies/api-rbac'));//注册api-rbac政策
